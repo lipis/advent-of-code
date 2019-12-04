bd23d2ed4a6073851e27653186aee2515172f6cc
Panayiotis Lipiridis <lipiridis@gmail.com>
2019-12-04 11:16:56 +0100

    04

---
+++ 2019/04.py
@@ -0,0 +1,46 @@
+def validate_part1(password):
+    same = False
+    increase = True
+    last_char = "0"
+    for char in str(password):
+        if char == last_char:
+            same = True
+        if last_char > char:
+            increase = False
+            break
+        last_char = char
+    return same and increase
+
+
+def validate_part2(password):
+    same = {}
+    increase = True
+    last_char = "0"
+    for char in str(password):
+        if char == last_char:
+            if char not in same:
+                same[char] = 1
+            same[char] += 1
+        if last_char > char:
+            increase = False
+            break
+        last_char = char
+
+    group = False
+    if same:
+        for char in same:
+            if same[char] == 2:
+                group = True
+
+    return increase and group
+
+
+part1 = 0
+part2 = 0
+for password in range(254032, 789860):
+    if validate_part1(password):
+        part1 += 1
+    if validate_part2(password):
+        part2 += 1
+
+print part1, part2
